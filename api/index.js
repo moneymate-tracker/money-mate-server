cb2e3e8d8e5c8dcff6fe15ac270c7322664fb00c
shams raza <shamsraza222333@gmail.com>
2026-05-20 23:37:09 +0530

    Add Vercel deployment config

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "../src/app.js";
+import connectDB from "../src/db/index.js";
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
